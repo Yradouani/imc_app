9583544a91cfddf35d53fdb98b38cad8a353751c
Yasmine Radouani <yasmine.radouani@outlook.fr>
2022-09-05 13:56:08 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const BMIData = [
+  { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
+  { name: "Bonne santé", color: "green", range: [18.5, 25] },
+  { name: "Surpoids", color: "lightcoral", range: [25, 30] },
+  { name: "Obésité modérée", color: "orange", range: [30, 35] },
+  { name: "Obésité sévère", color: "crimson", range: [35, 40] },
+  { name: "Obésité morbide", color: "purple", range: 40 },
+];
+
+// IMC = poids en kg / taille² en m
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", handleForm);
+function handleForm(e) {
+  e.preventDefault();
+
+  calculateBMI();
+}
+
+const inputs = document.querySelectorAll("input");
+
+function calculateBMI() {
+  const height = inputs[0].value;
+  const weight = inputs[1].value;
+  const bmi = ((weight) / Math.pow(height / 100, 2)).toFixed(1);
+  console.log(bmi);
+
+  if (height && weight && (height > 0) && (weight > 0)) {
+    handleResponse(bmi);
+    return;
+  } else {
+    handleError();
+    return;
+  }
+}
+
+const displayBMI = document.querySelector(".bmi-value");
+const result = document.querySelector(".result");
+
+function handleError() {
+  displayBMI.textContent = "Wops";
+  displayBMI.style.color = "inherit";
+  result.textContent = "Veuillez remplir correctement les champs";
+}
+
+function handleResponse(a) {
+  const rank = BMIData.find(data => {
+    if (a >= data.range[0] && a < data.range[1]) return data;
+    else if (typeof data.range === "number" && a >= data.range) return data;
+  })
+  displayBMI.textContent = a;
+  displayBMI.style.color = `${rank.color}`
+  result.textContent = `Résultat : ${rank.name}`;
+}
